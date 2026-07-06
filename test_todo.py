05b16d950e132394cbd343b6395e132730f49f1d
skndl-quality <skandaal01@gmail.com>
2026-07-06 16:31:54 +0300

    добавил функцию на фильтрацию элементов

--- test_todo.py
+++ test_todo.py
@@ -1,3 +1,5 @@
+from pydoc import pager
+
 import pytest
 
 
@@ -17,5 +19,30 @@ def test_locator(page):
     page.wait_for_timeout(2000)
     page.get_by_role("button", name="Company").click()
     page.wait_for_timeout(2000)
+    page.locator("li").filter(has_text='Company').click() #ФИЛЬТРАЦИЯ ПО ТЕКСТУ
+    page.wait_for_timeout(2000)
+    page.locator("li").filter(has=page.locator('.dropdown-toggle')).click() #ФИЛЬТРАЦИЯ ПО ЛОКАТОРУ
+    page.pause()
 
+
+@pytest.mark.filter
+def test_locator_filter(page):
+    page.goto('https://zimaev.github.io/filter/')
+
+# фильтрация по отсутствию роли в элемента
+    row_locator = page.locator('tr')
+    total = row_locator.filter(has_not=page.get_by_role("button")).count()
+    print(total)
+
+#фильтрация по отсутствию текста
+    row_locator_text = page.locator('tr')
+    total2 = row_locator_text.filter(has_not_text="helicopter").count()
+    print(total2)
+
+#фильтрация по тексту и кнопки, в итоге найдется 1 элемент, который подходит по двум условиям
+    row_locator_and = page.locator('tr')
+    total4 = (row_locator_and
+              .filter(has_text="QA")
+              .filter(has=page.get_by_role('button')).count())
+    print(total4)
     page.pause()
